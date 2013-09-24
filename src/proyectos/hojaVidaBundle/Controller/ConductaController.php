efc7b52fd01934571f599026464d8fcc3a063369
Luis Santos <lsantos@produccion.gob.pe>
2013-09-24 15:58:39 -0500

    hdsgfdsggds

---
+++ src/proyectos/hojaVidaBundle/Controller/ConductaController.php
@@ -0,0 +1,19 @@
+<?php
+namespace proyectos\hojaVidaBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\Request;
+
+
+class ConductaController extends Controller {
+ /**
+     * @Route("/frm_update_conducta",name="_frm_update_conducta")
+     */
+    public function direcAction() {
+       
+        return $this->render('hojaVidaBundle:update_formularios:frm_update_conducta.html.twig',array("programador"=>"cesar"));
+    }
+}
+?>
